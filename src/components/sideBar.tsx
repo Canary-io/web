1e2d6414e19772bb1226a7dc42449e47543c8962
Joseph Jophy <josephjophy@Josephs-iMac.local>
2026-04-15 11:21:40 -0500

    updates

--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -1,20 +1,46 @@
+import Image from "next/image";
+
 export default function Sidebar() {
-    return (
-      <div className="h-screen w-16 bg-gray-900 text-white flex flex-col justify-between p-4">
-        
-        
-        <div>
-          <h2 className="text-xl font-bold mb-6">Dashboard</h2>
-          <button className="w-full text-left p-2 rounded hover:bg-gray-800">
-            Profile
-          </button>
-          <button className="w-full text-left p-2 rounded hover:bg-gray-800">
-            + 
-          </button>
+  return (
+    <div className="h-screen w-16 bg-gray-900 text-white flex flex-col justify-between p-4">
+      <div>
+        <div className="mb-6 flex w-full justify-center">
+          <Image
+            src="/orca.png"
+            alt="Orca logo"
+            width={32}
+            height={32}
+            className="rounded-full object-cover"
+          />
         </div>
-  
-  
+        <button className="flex w-full justify-center p-2 rounded hover:bg-gray-800">
+          <Image
+            src="/home.svg"
+            alt="Home"
+            width={20}
+            height={20}
+            className="invert"
+          />
+        </button>
+        <button className="mt-2 flex w-full justify-center p-2 rounded hover:bg-gray-800">
+          <Image
+            src="/add.svg"
+            alt="Add"
+            width={20}
+            height={20}
+            className="invert"
+          />
+        </button>
       </div>
-    );
-  }
-  
+      <button className="flex w-full justify-center p-2 rounded hover:bg-gray-800">
+        <Image
+          src="/settings.svg"
+          alt="Settings"
+          width={20}
+          height={20}
+          className="invert"
+        />
+      </button>
+    </div>
+  );
+}
